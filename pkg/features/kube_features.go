66761573bfc480d11b5aae596f1b109c5615042f
Charles Eckman <cceckman@google.com>
2019-01-29 13:46:37 -0800

    Backport OIDC endpoints (#80724)
    This is a required feature for Workload Identity Anywhere, which is
    targeting Anthos 1.3 in Alpha feature state. Anthos 1.3 is shipping with
    1.15.7, so this feature is being backported to 1.15.7+ for the Alpha.
    
    Owners who can de-conflict changes: mtaufen@google.com,
    cjcullen@google.com.
    
    Original commit text:
    
    Provide OIDC discovery endpoints
    
    - Add handlers for service account issuer metadata.
    - Add option to manually override JWKS URI.
    - Add unit and integration tests.
    - Add a separate ServiceAccountIssuerDiscovery feature gate.
    
    Additional notes:
    - If not explicitly overridden, the JWKS URI will be based on
      the API server's external address and port.
    
    - The metadata server is configured with the validating key set rather
    than the signing key set. This allows for key rotation because tokens
    can still be validated by the keys exposed in the JWKs URL, even if the
    signing key has been rotated (note this may still be a short window if
    tokens have short lifetimes).
    
    - The trust model of OIDC discovery requires that the relying party
    fetch the issuer metadata via HTTPS; the trust of the issuer metadata
    comes from the server presenting a TLS certificate with a trust chain
    back to the from the relying party's root(s) of trust. For tests, we use
    a local issuer (https://kubernetes.default.svc) for the certificate
    so that workloads within the cluster can authenticate it when fetching
    OIDC metadata. An API server cannot validly claim https://kubernetes.io,
    but within the cluster, it is the authority for kubernetes.default.svc,
    according to the in-cluster config.
    
    Co-authored-by: Michael Taufen <mtaufen@google.com>
    Change-Id: Ib83e73158aad5037ab7adaca4223d76f0d6278f0

--- pkg/features/kube_features.go
+++ pkg/features/kube_features.go
@@ -272,6 +272,15 @@ const (
 	// to the API server.
 	BoundServiceAccountTokenVolume featuregate.Feature = "BoundServiceAccountTokenVolume"
 
+	// owner: @mtaufen
+	// alpha: v1.18
+	//
+	// Enable OIDC discovery endpoints (issuer and JWKS URLs) for the service
+	// account issuer in the API server.
+	// Note these endpoints serve minimally-compliant discovery docs that are
+	// intended to be used for service account token verification.
+	ServiceAccountIssuerDiscovery featuregate.Feature = "ServiceAccountIssuerDiscovery"
+
 	// owner: @Random-Liu
 	// beta: v1.11
 	//
@@ -551,6 +560,7 @@ var defaultKubernetesFeatureGates = map[featuregate.Feature]featuregate.FeatureS
 	TokenRequest:                        {Default: true, PreRelease: featuregate.Beta},
 	TokenRequestProjection:              {Default: true, PreRelease: featuregate.Beta},
 	BoundServiceAccountTokenVolume:      {Default: false, PreRelease: featuregate.Alpha},
+	ServiceAccountIssuerDiscovery:       {Default: false, PreRelease: featuregate.Alpha},
 	CRIContainerLogRotation:             {Default: true, PreRelease: featuregate.Beta},
 	deprecatedGCERegionalPersistentDisk: {Default: true, PreRelease: featuregate.GA, LockToDefault: true}, // remove in 1.17
 	CSIMigration:                        {Default: false, PreRelease: featuregate.Alpha},
