0f82ae7b1bce476a4afb02b4c2363af188302c9f
Jacek Kaniuk <jkaniuk@google.com>
2019-12-16 20:17:48 +0100

    Filter unanswerable SubjectAccessReviews in the webhook authorizer
    Duplicates part of hostedmaster logic to reduce number of calls.
    
    Bugs: b/142479954 b/142480707
    
    Change-Id: I0cc675d143b32ac651136fd435ec717e0bcd594a

--- pkg/kubeapiserver/authorizer/config.go
+++ pkg/kubeapiserver/authorizer/config.go
@@ -97,7 +97,7 @@ func (config Config) New() (authorizer.Authorizer, authorizer.RuleResolver, erro
 			authorizers = append(authorizers, abacAuthorizer)
 			ruleResolvers = append(ruleResolvers, abacAuthorizer)
 		case modes.ModeWebhook:
-			webhookAuthorizer, err := webhook.New(config.WebhookConfigFile,
+			webhookAuthorizer, err := webhook.NewGkeApiserverWebhookAuthorizer(config.WebhookConfigFile,
 				config.WebhookCacheAuthorizedTTL,
 				config.WebhookCacheUnauthorizedTTL)
 			if err != nil {
